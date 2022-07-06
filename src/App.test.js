bdb6a182e072f20daa009c45530af5e0caaf8b1c
Max Allfrey <max.allfrey@gmail.com>
2022-07-06 15:43:27 +0100

    Added randomise and clear buttons

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from "./App";
 describe("When viewing page", () => {
   it("Should generate a list", () => {
     const { getByTestId } = render(<App />);
-    const orderedList = getByTestId("namelist");
+    const orderedList = getByTestId("nameList");
     expect(orderedList).toBeInTheDocument();
   });
 });
@@ -26,7 +26,7 @@ describe("When viewing page", () => {
 
 describe("When adding a name to the list", () => {
   it("Should display a list containing the name entered", () => {
-    const { getByTestId, getByRole } = render(<App />);
+    const { getByTestId } = render(<App />);
     const name = "Placeholder";
     let nameInput = getByTestId("nameInput");
     fireEvent.change(nameInput, { target: { value: "Placeholder" } });
@@ -34,7 +34,53 @@ describe("When adding a name to the list", () => {
     let nameForm = getByTestId("nameform");
     fireEvent.submit(nameForm);
 
-    let nameList = getByTestId("namelist");
+    const nameList = getByTestId("nameList");
+
     expect(nameList).toHaveTextContent(name);
+  });
+});
+
+describe("When pressing the randomise button", () => {
+  it("Should randomise the existing list.", () => {
+    const { getByTestId } = render(<App />);
+
+    let nameInput = getByTestId("nameInput");
+    let nameForm = getByTestId("nameform");
+
+    for (let index = 0; index < 10; index++) {
+      fireEvent.change(nameInput, { target: { value: `Placeholder ${index}` } });
+      fireEvent.submit(nameForm);
+    }
+
+    const nameList = getByTestId("nameList");
+    console.log(nameList)
+
+    const randomButton = getByTestId("randomButton");
+    fireEvent.click(randomButton);
+
+    const randomisedNameList = getByTestId("nameList");
+
+    console.log(nameList);
+    expect(nameList).not.toEqual(randomisedNameList);
+  });
+});
+
+
+describe("When clicking the clear button", () => {
+  it("Should clear the existing list", () => {
+    const { getByTestId } = render(<App />);
+
+    let nameInput = getByTestId("nameInput");
+    let nameForm = getByTestId("nameform");
+
+    fireEvent.change(nameInput, { target: { value: "Placeholder" } });
+    fireEvent.submit(nameForm);
+    
+    const clearButton = getByTestId("clearButton");
+    fireEvent.click(clearButton);
+
+    const nameList = getByTestId("nameList");
+
+    expect(nameList).not.toHaveTextContent("Placeholder");
   });
 });
