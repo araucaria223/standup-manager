8d552f7c940b0fc96522696c96a2a74397e2b8ec
Max Allfrey <max.allfrey@gmail.com>
2022-07-08 12:17:18 +0100

    Added copy to clipboard button

--- src/App.js
+++ src/App.js
@@ -37,6 +37,14 @@ function App() {
     setNames([ ...names ]);
   }
 
+  function handleCopy(event) {
+    let copyText = "";
+    names.forEach((name, index) => {
+      copyText += `${index + 1}. ${name}\n`
+    })
+    navigator.clipboard.writeText(copyText);
+  }
+
   return (
     <>
       <div className="content-wrapper">
@@ -70,6 +78,9 @@ function App() {
           </button>
           <button type="button" className="clear-button" data-testid="clearButton" onClick={(event) => handleClear(event)}>
             <svg height="18" width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg>
+          </button>
+          <button type="button" className="copy-button" data-testid="copyButton" onClick={(event) => handleCopy(event)}>
+            <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M384 96L384 0h-112c-26.51 0-48 21.49-48 48v288c0 26.51 21.49 48 48 48H464c26.51 0 48-21.49 48-48V128h-95.1C398.4 128 384 113.6 384 96zM416 0v96h96L416 0zM192 352V128h-144c-26.51 0-48 21.49-48 48v288c0 26.51 21.49 48 48 48h192c26.51 0 48-21.49 48-48L288 416h-32C220.7 416 192 387.3 192 352z"/></svg>
           </button>
         </div>
         <ol data-testid="nameList">
