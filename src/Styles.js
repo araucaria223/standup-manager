7f43f979e8f6b5245d0afd1c4f379cc5b462a1bb
Max Allfrey <max.allfrey@gmail.com>
2022-07-13 16:01:31 +0100

    Finished refactoring styles to styled components

--- src/Styles.js
+++ src/Styles.js
@@ -1,4 +1,37 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(props) => props.theme.colors.base};
+    color: ${(props) => props.theme.colors.text};
+  }
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const TitleWrapper = styled.div`
+  text-align: center;
+  margin-bottom: 0.5rem;
+
+  & h1 {
+    margin-bottom: 0.2rem;
+  }
+
+  & span {
+    color: ${(props) => props.theme.colors.subtext1};
+  }
+`;
+
+export const NavWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+`;
 
 export const AddButton = styled.button`
   background-color: ${(props) => props.theme.colors.sapphire};
@@ -24,14 +57,23 @@ export const NameInput = styled.input`
 
   padding: 0.5rem;
 
-  &:placeholder {
+  &::placeholder {
     color: ${(props) => props.theme.colors.overlay1};
     opacity: 1;
   }
 `;
 
-const ActionButton = styled.button`
+export const ActionButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  margin: 1rem;
+`;
+
+export const ActionButton = styled.button`
+  opacity: ${(props) => (props.visible === "on-hover" ? 0 : 1)};
   background-color: ${(props) => props.theme.colors[props.color]};
+  color: ${(props) => props.theme.colors.surface0};
   border: 2px solid transparent;
   border-radius: 20px;
   margin-left: 0.5rem;
@@ -48,4 +90,45 @@ const ActionButton = styled.button`
   }
 `;
 
-export { ActionButton };
+export const NameList = styled.ol`
+  min-width: 20rem;
+  margin: auto;
+  counter-reset: item;
+  list-style-type: none;
+  padding-left: 0;
+  border: 2px solid ${(props) => props.theme.colors.overlay1};
+  border-radius: 16px;
+
+  &:empty {
+    border: none;
+  }
+`;
+
+export const NameItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0.5rem 1rem;
+  box-sizing: border-box;
+  border-bottom: 2px solid ${(props) => props.theme.colors.overlay1};
+
+  & span {
+    margin-inline: 0.5rem;
+  }
+
+  &:hover .item-remove-button,
+  &:focus-within .item-remove-button {
+    transition-delay: 0.1s;
+    transition-duration: 0.3s;
+    opacity: 1;
+  }
+
+  &:before {
+    content: counter(item) "  ";
+    counter-increment: item;
+  }
+
+  &:last-child {
+    border: none;
+  }
+`;
