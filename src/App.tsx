9737cb944ae63a713a7f1cc65fdb7231cde50c81
Max Allfrey <max.allfrey@gmail.com>
2022-07-17 21:16:28 +0100

    Added typescript files

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme";
+
+function App() {
+  return <h1>Hello world</h1>;
+}
+
+export default App;
