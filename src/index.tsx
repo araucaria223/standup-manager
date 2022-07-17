2d2d93421e25b67c97bc0c974bb69f84c175e8d5
Max Allfrey <max.allfrey@gmail.com>
2022-07-17 22:52:34 +0100

    Fixed index.tsx using deprecated functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
 
-import "./index.css";
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
